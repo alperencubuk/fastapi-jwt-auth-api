5cfd0f2efa5e05149ecf83572d9bf30e42071ed5
Alperen Çubuk <alperencubuk@outlook.com>
2023-11-25 21:23:58 +0300

    Optimize services and views (#3)

--- source/app/users/services.py
+++ source/app/users/services.py
@@ -1,4 +1,3 @@
-from asyncio import gather
 from math import ceil
 
 from sqlalchemy import asc, desc, func, select
@@ -11,42 +10,35 @@ from source.app.users.schemas import (
     UserCreate,
     UserPage,
     UserRequest,
-    UserResponse,
     UserUpdate,
     UserUpdateRequest,
 )
 
 
-async def get_user(user_id: int, db: AsyncSession) -> UserResponse | None:
-    if user := await db.get(User, user_id):
-        return UserResponse.model_validate(user)
-    return None
-
-
-async def create_user(user: UserRequest, db: AsyncSession) -> UserResponse | None:
+async def create_user(user: UserRequest, db: AsyncSession) -> User | None:
     try:
         user = User(**UserCreate(**user.model_dump()).model_dump())
         db.add(user)
         await db.commit()
         await db.refresh(user)
-        return UserResponse.model_validate(user)
+        return user
     except IntegrityError:
         return None
 
 
 async def update_user(
     user: User,
-    payload: UserUpdateRequest,
+    request: UserUpdateRequest,
     db: AsyncSession,
-) -> UserResponse | None:
+) -> User | None:
     try:
-        fields_to_update = UserUpdate(**payload.model_dump()).model_dump().items()
+        fields_to_update = UserUpdate(**request.model_dump()).model_dump().items()
         for key, value in fields_to_update:
             if value is not None:
                 setattr(user, key, value)
         await db.commit()
         await db.refresh(user)
-        return UserResponse.model_validate(user)
+        return user
     except IntegrityError:
         return None
 
@@ -61,15 +53,16 @@ async def list_users(
     page: int, size: int, sort: Sort, order: Order, db: AsyncSession
 ) -> UserPage:
     order = asc(sort) if order == Order.ASC else desc(sort)
-    users, total = await gather(
-        db.scalars(select(User).order_by(order).offset((page - 1) * size).limit(size)),
-        db.scalar(select(func.count(User.id))),
+
+    users = await db.scalars(
+        select(User).order_by(order).offset((page - 1) * size).limit(size)
     )
-    users_list = [UserResponse.model_validate(user) for user in users.all()]
+    total = await db.scalar(select(func.count(User.id)))
+
     return UserPage(
-        users=users_list,
+        users=users,
         page=page,
         size=size,
         total=total,
-        pages=(ceil(total / size) if size else 1),
+        pages=(ceil(total / size)),
     )
